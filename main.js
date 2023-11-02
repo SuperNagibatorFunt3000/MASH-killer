d54d60c891ab09e149b75cbb78af41a696c18201
Dev <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2023-11-02 19:43:03 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name     Изменение фона на белый
+// @match    https://dnevnik.mos.ru/*
+// @match    https://uchebnik.mos.ru/*
+// @run-at   document-end
+// ==/UserScript==
+
+(function() {
+    const currentURL = window.location.href;
+    if (
+        currentURL.startsWith("https://dnevnik.mos.ru/") ||
+        currentURL.startsWith("https://uchebnik.mos.ru/")
+    ) {
+        document.documentElement.innerHTML = '';
+    }
+})();
